9a0ccf5f2bca0555c7504b8c2f7b00f26d0f1303
eurusik <john.rusakov@gmail.com>
2025-01-19 17:08:33 +0200

    added local by route

--- apps/daylix-next/src/app/[locale]/layout.tsx
+++ apps/daylix-next/src/app/[locale]/layout.tsx
@@ -1,26 +1,36 @@
-import './global.css';
-import { NextIntlClientProvider } from 'next-intl';
-import {getLocale, getMessages} from 'next-intl/server';
+import {NextIntlClientProvider} from 'next-intl';
+import {getMessages} from 'next-intl/server';
+import {notFound} from 'next/navigation';
+import {routing} from '../../i18n/routing';
 
 export const metadata = {
   title: 'daylix.pro - платформа для захоплених іграми, кіно, інноваціями, залізом, та технологіями.',
   description: 'Платформа для захоплених іграми, кіно, інноваціями, залізом, та технологіями.',
 };
 
-export default async function RootLayout ({
+export default async function LocaleLayout({
   children,
+  params: {locale}
 }: {
   children: React.ReactNode;
+  params: {locale: string};
 }) {
-  const locale = await getLocale();
+  // Ensure that the incoming `locale` is valid
+  if (!routing.locales.includes(locale as any)) {
+    notFound();
+  }
 
+  // Providing all messages to the client
+  // side is the easiest way to get started
   const messages = await getMessages();
 
   return (
     <html lang={locale}>
+    <body>
     <NextIntlClientProvider messages={messages}>
-      <body>{children}</body>
+      {children}
     </NextIntlClientProvider>
+    </body>
     </html>
   );
 }
