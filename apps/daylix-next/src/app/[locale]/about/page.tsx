cbafbd2992252c913b6d8a287284f94f9aa3b766
eurusik <john.rusakov@gmail.com>
2025-01-30 23:14:19 +0200

    added revalidate to about
    added no-cache to middleware

--- apps/daylix-next/src/app/[locale]/about/page.tsx
+++ apps/daylix-next/src/app/[locale]/about/page.tsx
@@ -1,6 +1,8 @@
 import ClientBlocksRenderer from './сlient-blocks-renderer';
 import { getAboutData } from './about-api';
 
+export const revalidate = 60;
+
 export default async function AboutPage({ params }: { params: { locale: string } }) {
   const about = await getAboutData(params.locale);
 
--- apps/daylix-next/src/middleware.ts
+++ apps/daylix-next/src/middleware.ts
@@ -1,9 +1,30 @@
-import createMiddleware from 'next-intl/middleware';
-import {routing} from './i18n/routing';
+import createIntlMiddleware from 'next-intl/middleware';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { routing } from './i18n/routing';
 
-export default createMiddleware(routing);
+export default function middleware(request: NextRequest) {
+  const intlHandler = createIntlMiddleware(routing);
+
+  const response = intlHandler(request);
+
+  if (!(response instanceof Response)) {
+    return response;
+  }
+
+  const requestHeaders = new Headers(request.headers);
+
+  const newResponse = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+
+  newResponse.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+
+  return newResponse;
+}
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(uk|ru)/:path*']
+  matcher: ['/', '/(uk|ru)/:path*'],
 };
